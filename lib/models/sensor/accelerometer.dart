f3e1cbed7e196b1f3b772b50e62aee826506099f
Nam <tranhuunam23022000@gmail.com>
2022-07-11 23:45:18 +0700

    chart

---
+++ lib/models/sensor/accelerometer.dart
@@ -0,0 +1,16 @@
+import 'dart:ffi';
+
+class AccelerometerModel {
+  final double x;
+  final double y;
+  final double z;
+  final DateTime time;
+  AccelerometerModel(
+      {required this.x, required this.y, required this.z, required this.time});
+}
+
+class AccelerometerChartModel {
+  final double value;
+  final DateTime time;
+  AccelerometerChartModel({required this.value, required this.time});
+}
